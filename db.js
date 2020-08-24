fab3a521a51b4b1a675bf06c17459b3d6890c2bd
Brendan Barr <bbarr1384@gmail.com>
2020-08-24 14:37:53 -0700

    with basic buildout

--- db.js
+++ db.js
@@ -3,16 +3,90 @@ const sqlite3 = require('better-sqlite3')
 
 const db = sqlite3(process.env.DATABASE_PATH)
 
-const selectUserByEmail = db.prepare('select id, email, hashedpassword from users where email=?')
-const selectUserById = db.prepare('select id, email, hashedpassword from users where id=?')
-const insertUser = db.prepare('insert into users (email, hashedpassword) values (@email, @hashedpassword)')
-const selectPostsByUserId = db.prepare('select id, content, title from posts where userid=?')
+function camelToSnake(key) {
+  return key.replace(/([A-Z])/g, '_$1').toLowerCase();
+}
+
+function snakeToCamel(key) {
+  return key.replace(/(_\w)/g, match => match.substr(1).toUpperCase())
+}
+
+function transformKeys(transformer, input) {
+  const output = {}
+  for (const key of Object.keys(input)) {
+    output[transformer(key)] = input[key];
+  }
+  return output
+}
+
+const snakeKeys = transformKeys.bind(null, camelToSnake)
+const camelKeys = transformKeys.bind(null, snakeToCamel)
+
+// user
+const selectUserByEmail = db.prepare('select id, email, hashed_password from user where email=?')
+const selectUserById = db.prepare('select id, email from user where id=?')
+const insertUser = db.prepare('insert into user (name, email, hashed_password) values (@name, @email, @hashed_password)')
+
+// site
+const selectSiteByHandle = db.prepare('select name, handle from site where handle=?')
+const selectDefaultSiteByUserId = db.prepare(`
+  select 
+    s.handle 
+  from 
+    site s,
+    user_site_role r 
+  where 
+    r.user_id=? and
+    r.site_handle=s.handle
+  limit 1;
+  `)
+const insertSite = db.prepare(`
+  insert into site
+    (name, handle, billing_subscription_id, billing_is_active) 
+  values 
+    (@name, @handle, @billing_subscription_id, @billing_is_active)
+`)
+const updateSiteBilling = db.prepare(`
+  update site 
+     set 
+         billing_subscription_id=@billing_subscription_id, 
+         billing_is_active=@billing_is_active 
+   where handle=@handle
+`)
+
+// post
+const selectPostsBySiteHandle = db.prepare('select id, content, title, created_at, updated_at, published_at from post where site_handle=?')
+const selectPostContentById = db.prepare('select content from post where id=?')
+const selectPostById = db.prepare('select * from post where id=?')
+
+// role
+const insertRole = db.prepare('insert into user_site_role (user_id, site_handle, role) values (@user_id, @site_handle, @role)')
+
 
 module.exports = {
-  userByEmail: email => selectUserByEmail.get(email),
-  createUser: db.transaction(props => {
-    const info = insertUser.run(props)
-    return selectUserById.get(info.lastInsertRowid)
-  }),
-  postsByUserId: id => selectPostsByUserId.all(id)
+  users: {
+    byEmail(email) {
+      return selectUserByEmail.get(email)
+    },
+    create: db.transaction(props => {
+      const info = insertUser.run(snakeKeys(props))
+      return selectUserById.get(info.lastInsertRowid)
+    })     
+  },
+  sites: {
+    defaultByUserId: id => selectDefaultSiteByUserId.get(id),
+    create: db.transaction(props => {
+      insertSite.run(snakeKeys(props))
+      insertRole.run(snakeKeys({ ...props, siteHandle: props.handle }))
+      return selectSiteByHandle.get(props.handle)
+    }),
+    updateBilling(props) {
+      return updateSiteBilling.run(snakeKeys(props))
+    }
+  },
+  posts: {
+    bySiteHandle: handle => selectPostsBySiteHandle.all(handle).map(camelKeys),
+    byId: id => camelKeys(selectPostById.get(id)),
+    contentById: id => camelKeys(selectPostContentById.get(id))
+  }
 }
--- server.js
+++ server.js
@@ -17,30 +17,74 @@ const server = express()
 const liquid = new Liquid();
 
 server.engine('liquid', liquid.express())
+server.use(express.static(`${__dirname}/assets`))
 server.use(bodyParser.json())
 server.use(cookieParser(process.env.COOKIE_SECRET))
 server.set('view engine', 'liquid');
 
-async function requireAuth(req, res, next) {
+const respond = (res, status, body) => {
+  const payload = status > 199 && status < 400 ? { data: body, error: null } : { data: null, error: body }
+  res.status(status).send(payload)
+}
 
-  const jwt = req.signedCookies.auth
-  console.log(req.signedCookies)
-
-  try {
-    const { id } = await jwtVerifyP(jwt, process.env.JWT_SECRET)
-    res.locals.userId = id
-    next()
-  } catch(e) {
-    return res.redirect('/')
+const safeRoute = fn => {
+  return (req, res) => {
+    try {
+      fn(req, res)
+    } catch(e) {
+      res.status(400).send(e.message)
+    }
   }
+}
+
+const safe = (fn, ...args) => {
+  try {
+    return [ fn(...args), null ]
+  } catch(e) {
+    return [ null, e ]
+  }
+}
+
+const safeP = p => p.then(
+  d => [ d, null ],
+  e => [ null, e ]
+)
+
+
+server.use(async (req, res, next) => {
+  try {
+    const jwt = req.signedCookies.auth
+    const { userId, siteHandle } = await jwtVerifyP(jwt, process.env.JWT_SECRET)
+    console.log('auth!', userId, siteHandle)
+    res.locals.userId = userId
+    res.locals.siteHandle = siteHandle
+  } catch(e) {}
+  next()
+})
+
+function requireUser(req, res, next) {
+  if (!res.locals.userId)
+    return res.redirect('/login')
+  if (res.locals.userId && !res.locals.siteHandle)
+    return res.redirect('/signup')
+  next()
 }
 
 server.get('/', (req, res) => {
   res.render('index.liquid')
 })
 
-server.get('/dashboard', requireAuth, (req, res) => {
-  res.render('dashboard.liquid', { posts: [] })
+server.get('/dashboard', requireUser, (req, res) => {
+
+  const posts = db.posts.bySiteHandle(res.locals.siteHandle)
+
+  res.render('dashboard.liquid', {
+    posts
+  })
+})
+
+server.get('/admin/settings', requireUser, (req, res) => {
+  res.render('settings.liquid', { sites: [] })
 })
 
 server.get('/login', (req, res) => {
@@ -48,7 +92,21 @@ server.get('/login', (req, res) => {
 })
 
 server.get('/signup', (req, res) => {
-  res.render('signup.liquid')
+  console.log('signup is user', !!res.locals.userId)
+  res.render('signup.liquid', {
+    isUser: !!res.locals.userId
+  })
+})
+
+server.get('/editor/:id', (req, res) => {
+  const post = db.posts.byId(req.params.id)
+  res.render('editor.liquid', {
+    post
+  })
+})
+
+server.get('/api/posts/:id/content', (req, res) => {
+  respond(res, 200, db.posts.contentById(req.params.id).content)
 })
 
 server.post('/api/posts', (req, res) => {
@@ -71,30 +129,91 @@ server.post('/api/login', async (req, res) => {
 
   const { email, password } = req.body
 
-  const { id, hashedpassword } = db.userByEmail(email)
+  const { id, hashed_password: hashedPassword } = db.users.byEmail(email)
   if (!id) return res.sendStatus(404) 
 
-  if (await bcrypt.compare(password, hashedpassword)) {
-    res.cookie('auth', await jwtSignP({ id }, process.env.JWT_SECRET), { httpOnly: true, signed: true })
-    return res.sendStatus(200)
+  console.log(password, hashedPassword)
+  if (await bcrypt.compare(password, hashedPassword)) {
+    const { handle } = db.sites.defaultByUserId(id)
+    if (!handle) {
+      res.cookie('auth', await jwtSignP({ userId: id }, process.env.JWT_SECRET), { httpOnly: true, signed: true })
+    } else {
+      res.cookie('auth', await jwtSignP({ userId: id, siteHandle: handle }, process.env.JWT_SECRET), { httpOnly: true, signed: true })
+    }
+    return respond(res, 200)
   }
 
-  return res.sendStatus(404)
+  return respond(res, 404)
 })
 
-server.post('/api/signup', async (req, res) => {
+server.post('/api/create-user', async (req, res) => {
 
-  const { email, password } = req.body
+  const { name, email, password } = req.body
 
-  const hashedpassword = await bcrypt.hash(password, 10 /* salt rounds */)
+  const hashedPassword = await bcrypt.hash(password, 10 /* salt rounds */)
 
-  try {
-    const { id } = db.createUser({ email, hashedpassword })
-    res.cookie('auth', await jwtSignP({ id }, process.env.JWT_SECRET), { signed: true, httpOnly: true })
-    res.sendStatus(201)
-  } catch(e) {
-    res.status(400).send('Something went wrong!')
-  }
+  const [ user, userE ] = safe(db.users.create, { 
+    name,
+    email, 
+    hashedPassword,
+  })
+
+  if (userE)
+    return respond(res, 400, userE.message)
+
+  res.cookie('auth', await jwtSignP({ userId: user.id }, process.env.JWT_SECRET), { httpOnly: true, signed: true })
+
+  respond(res, 201, user)
+})
+
+server.post('/api/create-site', async (req, res) => {
+  if (!res.locals.userId)
+    return respond(res, 400)
+
+  const { name, handle, stripeToken } = req.body
+
+  const [ site, siteE ] = safe(db.sites.create, { 
+    billingSubscriptionId: '',
+    billingIsActive: 1,
+    name,
+    handle,
+    userId: res.locals.userId,
+    role: 'owner'
+  })
+
+  if (siteE) 
+    return respond(res, 400, siteE.message)
+
+  /*
+  const [ stripeCustomer, stripeCustomerE ] = await safeP(
+    stripe.customers.create({
+      name,
+      email,
+      source: stripeToken
+    })
+  )
+  if (stripeCustomerE)
+    console.log('error making stripe customer!', stripeCustomerE.message)
+
+  const [ stripeSubscription, stripeSubscriptionE ] = await safeP(
+    tripe.subscriptions.create({
+      customer: customer.id,
+      items: [ { price: process.env.STRIPE_BASIC_PLAN_PRICE_ID } ]
+    })
+  )
+  if (stripeSubscriptionE)
+    console.log('error making stripe subscription!', stripeSubscriptionE.message)
+
+  const [ updatedSite, updatedSiteE ] = safe(db.updateSite, {
+    handle: site.id,
+    billingSubscriptionId: stripeSubscription.id,
+    billingIsActive: stripeSubscription.status === 'active'
+  })
+  */
+
+  res.cookie('auth', await jwtSignP({ userId: res.locals.userId, siteHandle: site.handle }, process.env.JWT_SECRET), { httpOnly: true, signed: true })
+
+  respond(res, 201, site)
 })
 
 module.exports = server
